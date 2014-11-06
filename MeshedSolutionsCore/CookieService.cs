19b4e9f9c26d375d00a19b4b304b417b3ddf6800
Mark Brown <marksbrown@gmail.com>
2014-11-06 18:04:12 -0500

    Add application support to cookie service

--- MeshedSolutionsCore/CookieService.cs
+++ MeshedSolutionsCore/CookieService.cs
@@ -5,29 +5,29 @@ namespace MeshedSolutionsCore
 {
     public static class CookieService
     {
-        public static string GetCookie(HttpContext context, string cookieName)
+        public static string GetCookie(HttpContext context, string applicationName, string cookieName)
         {
-            var httpCookie = HttpContext.Current.Request.Cookies["meatgrinder"];
+            var httpCookie = HttpContext.Current.Request.Cookies[applicationName];
             if (httpCookie != null)
                 return httpCookie[cookieName];
 
             return null;
         }
-        public static void SetCookie(HttpResponseBase response, string cookieName, int cookieDuration, string cookieValue = "0")
+        public static void SetCookie(HttpResponseBase response, string applicationName, string cookieName, int cookieDuration, string cookieValue = "0")
         {
-            var cookie = new HttpCookie("MeatGrinder");
+            var cookie = new HttpCookie(applicationName);
 
             cookie[cookieName] = cookieValue;
             cookie.Expires = DateTime.Now.AddDays(cookieDuration);
             response.Cookies.Add(cookie);
         }
-        public static void DeleteCookie(HttpResponseBase response, string cookieName)
+        public static void DeleteCookie(HttpResponseBase response, string applicationName, string cookieName)
         {
-            SetCookie(response, cookieName, -1);
+            SetCookie(response, applicationName, cookieName, -1);
         }
-        public static int GetUserID()
+        public static int GetUserID(string applicationName)
         {
-            int userID = int.Parse(CookieService.GetCookie(System.Web.HttpContext.Current, "UserID"));
+            int userID = int.Parse(GetCookie(HttpContext.Current, applicationName, "UserID"));
             return userID;
         }
 
