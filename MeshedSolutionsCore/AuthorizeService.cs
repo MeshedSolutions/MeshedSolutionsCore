28c9279d2cadf05a1141a843f0fca6817b8c9564
Mark Brown <marksbrown@gmail.com>
2014-11-09 21:31:55 -0500

    Update structure and add authorize param

---
+++ MeshedSolutionsCore/AuthorizeService.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+
+namespace MeshedSolutionsCore
+{
+    public class CustomAuthorize : AuthorizeAttribute
+    {
+        public string ApplicationName { get; set; }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            int userID = 0;
+
+            var cookieValue = CookieService.GetCookie(HttpContext.Current, ApplicationName, "UserID");
+            if (cookieValue != null)
+                userID = int.Parse(cookieValue);
+
+            return userID != 0;
+        }
+    }
+}
